3011c74c849d4d99a5c3c89adb3fac821d6f98e8
LuciaGojeiu <lucia.gojeiu@gmail.com>
2019-08-17 21:51:42 +0300

    Serenity 17.08.2019

---
+++ src/test/java/org/fasttrackit/features/LoginTest.java
@@ -0,0 +1,30 @@
+package org.fasttrackit.features;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.fasttrackit.steps.LoginSteps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+  public class LoginTest {
+    @Managed(uniqueSession = true)
+    private WebDriver driver;
+
+@Steps
+    private LoginSteps loginSteps;
+
+   private String userEmail="madisonqa16@gmail.com";
+   private String userPass="123abc";
+   private String userName="Madison Ella Smith";
+
+@Test
+ public void loginTest(){
+    loginSteps.navigateToLoginPage();
+    loginSteps.performLogin(userEmail, userPass);
+    loginSteps.checkLoggedIn(userName);
+   }
+
+}
